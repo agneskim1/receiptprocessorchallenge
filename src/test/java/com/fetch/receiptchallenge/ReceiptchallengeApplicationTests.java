7cd3d1dda9474b876e3844f3ccf32ae10ce03db5
agneskim1 <agneskim124@gmail.com>
2024-06-16 18:21:32 -0400

    Initialize springboot java application

---
+++ src/test/java/com/fetch/receiptchallenge/ReceiptchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.receiptchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
